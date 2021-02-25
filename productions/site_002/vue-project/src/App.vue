ff7da08daa3107a85155122e619c88a37d99df33
TakuyaKumada <looow.tk@gmail.com>
2021-02-25 14:43:47 +0900

    一時保存

--- productions/site_002/vue-project/src/App.vue
+++ productions/site_002/vue-project/src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <div id="app">
+      <router-view/>
+    </div>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
--- productions/site_002/vue-project/src/views/About.vue
+++ productions/site_002/vue-project/src/views/About.vue
@@ -1,18 +1,48 @@
 <template>
   <div id="home" class="big-bg">
+    <h1>Would you like to give shape to your memories?</h1>
+    <v-btn
+      large
+      min-width="150"
+      :loading="loading3"
+      :disabled="loading3"
+      color="blue-grey"
+      class="ma-2 white--text"
+      @click="loader = 'loading3'"
+    >
+      Registration
+    </v-btn>
+    <v-btn
+      large
+      min-width="200"
+      :loading="loading3"
+      :disabled="loading3"
+      color="blue-grey"
+      class="ma-2 white--text"
+      @click="loader = 'loading3'"
+    >
+      login
+    </v-btn>
   </div>
 </template>
 
 <style lang="scss" scoped>
 @import '../assets/ress/ress.min.css';
 /* 大きな背景画像 */
+#home {
+    background-image: url(../assets/images/image_1.jpg);
+    min-height: 100vh;
+    text-align: center;
+    padding-top: 40vh;
+}
 .big-bg {
     background-size: cover;  /*縦横比を固定したまま画面いっぱいに画像を広げる*/
     background-position: center top;  /*画像を中央上部に配置*/
     background-repeat: no-repeat;  /*画像を繰り返さない*/
-}
-#home {
-    background-image: url(../assets/images/image_1.jpg);
-    min-height: 100vh;
-}
+} 
+h1 {
+  color: #FFF;
+  font-size: 3rem;
+  padding-bottom: 30px;
+}   
 </style>
